39a8b4dea1d78a3b3c1cb349df7d0e08fd32c38b
Wei Wei <weiweisjtu@gmail.com>
2020-07-01 14:01:58 -0700

    Initial Commit

---
+++ BlockSite/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BlockSite
+//
+//  Created by wwei on 7/1/2020.
+//  Copyright © 2020 wwei. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+}
---
+++ BlockSite/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  BlockSite
+//
+//  Created by wwei on 7/1/2020.
+//  Copyright © 2020 wwei. All rights reserved.
+//
+
+import Cocoa
+import SafariServices.SFSafariApplication
+
+class ViewController: NSViewController {
+
+    @IBOutlet var appNameLabel: NSTextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.appNameLabel.stringValue = "BlockSite";
+    }
+    
+    @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "com.wwei10.BlockSite-Extension") { error in
+            if let _ = error {
+                // Insert code to inform the user that something went wrong.
+
+            }
+        }
+    }
+
+}
